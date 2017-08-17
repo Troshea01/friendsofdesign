8a3efe8ce63f444bb069919128f02e79586bd695
stevene <stevene@FOD-CCLKDNCR.local>
2017-08-17 14:53:36 +0200

    Working with FullPageJS

--- javascriptandjquery-14082017/js/application.js
+++ javascriptandjquery-14082017/js/application.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({});
+  var mainMenu = $('#main-menu');
+
+  $('#fullpage').fullpage();
+
+  $('#fullpage .section').each(function(){
+    var title = $(this).attr('data-title');
+    var mainMenuHtml = mainMenu.html();
+    var sectionAnchor = $(this).attr('data-anchor');
+
+    mainMenuHtml += `<li><a data-section-anchor='${sectionAnchor}'>${title}</a><ul>`;
+
+    $(this).find('.slide').each(function(){
+      var title = $(this).attr('data-title');
+      var slideAnchor = $(this).attr('data-anchor');
+  
+      mainMenuHtml += `<li><a data-section-anchor='${sectionAnchor}' data-slide-anchor='${slideAnchor}'>${title}</a></li>`;
+  
+      mainMenu.html(mainMenuHtml);
+    })
+
+    mainMenuHtml += `</ul></li>`;
+
+    mainMenu.html(mainMenuHtml);
+  })
+
+  mainMenu.on('click', 'a', function(event){
+    var slideAnchor = event.target.attr('data-slide-anchor');
+    var sectionAnchor = event.target.attr('data-section-anchor');
+
+    $.fn.fullpage.moveTo(slideAnchor, sectionAnchor);
+  });
 });
