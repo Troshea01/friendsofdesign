753b00f0f97d1196b36b4e5d73cd9444995ade75
Steven Eksteen <eksoverzero@outlook.com>
2017-09-14 10:21:14 +0200

    VueJS conditions and loops

--- precompiledtechnologies/js/application.js
+++ precompiledtechnologies/js/application.js
@@ -1,6 +1,21 @@
+var users = []
+var templates = [
+  { name: 'Dark', value: 'css/dark.css'},
+  { name: 'Light', value: 'css/light.css'}
+]
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    title: new Date(),
+    title: "Title",
+    templates: {
+      list: templates,
+      can_list: templates.length > 1
+    }
   }
 })
+
+new Chartist.Pie('.ct-chart', {
+  series: [5, 3, 4]
+}, {});
