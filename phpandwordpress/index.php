0cf9c51980a2739df226eb0e3ba5f060d3a2bc32
Steven Eksteen <eksoverzero@outlook.com>
2017-10-05 11:12:21 +0200

    Functions and URL params

--- phpandwordpress/index.php
+++ phpandwordpress/index.php
@@ -1,11 +1,18 @@
 <?php
-	$title = "The title of my page";
+  $title = "The title of my page";
   $templates = array(
     array('id' => 1, 'name' => 'Dark', 'value' => 'css/dark.css'),
     array('id' => 2, 'name' => 'Light', 'value' => 'css/light.css')
   );
-
   $default_template = 1;
+
+  # Set the template ID to the default template ID
+  # If a template ID has ben sent in the request, use
+  # that ID instead
+  $template = $default_template;
+  if (isset($_GET['template'])) {
+    $template = $_GET['template'];
+  };
 ?>
 
 <!DOCTYPE html>
@@ -19,12 +26,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-  	<h1><?php echo $title; ?></h1>
+    <h1><?php echo $title; ?></h1>
 
     <?php for ($i=0; $i < count($templates); $i++) { ?>
       <?php $template = $templates[$i]; ?>
 
-      <button v-on:click="setTemplate(template.id)">
+      <button onclick="setTemplate(template['id'])">
         <?php echo $template['name']; ?>
       
         <?php if ($template['id'] == $default_template) { ?>  
